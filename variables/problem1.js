5605cbf9318639866a6d49833aacc1d2e18be535
unknown <athmakuruharshitha@gmail.com>
2023-01-03 10:12:50 +0530

    second

---
+++ variables/problem1.js
@@ -0,0 +1,6 @@
+let name="Harshitha"
+console.log(name)
+name="Raghupathi Reddy"
+console.log(name)
+name="Nirmala"
+console.log(name)
---
+++ variables/problem3.js
@@ -0,0 +1,12 @@
+let name="Harshitha"
+let school_name="Z.P.H.S GLS(form)"
+let grade="A+"
+let maths_score=68;
+let telugu_score=98;
+let social_score=82;
+console.log(name)
+console.log(school_name);
+console.log(grade)
+console.log("maths",+maths_score)
+console.log("telugu",+telugu_score)
+console.log("social",+social_score)
